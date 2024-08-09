eeb8a9d322c116a3296eff557c9ff80c15d400ba
Pedro-Landin-de-Oliveira <henriquep35036@gmail.com>
2024-08-09 15:47:15 -0300

    Cabeçalho

--- space-app/src/App.jsx
+++ space-app/src/App.jsx
@@ -1,5 +1,6 @@
 import { styled } from "styled-components";
 import EstilosGlobais from "./componentes/EstilosGlobais";
+import Cabecalho from "./componentes/Cabecalho";
 
 
 const FundoGradiente = styled.div`
@@ -16,6 +17,7 @@ const FundoGradiente = styled.div`
 function App() {
   return <FundoGradiente>
    <EstilosGlobais />
+   <Cabecalho />
   </FundoGradiente>;
 }
 
